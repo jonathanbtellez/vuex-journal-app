c4056a2ed82016e07ca9eccb279e408e9aaa8e80
jonathanbtellez <104452285+jonathanbtellez@users.noreply.github.com>
2023-07-29 04:49:06 +0000

    Mostrar un loading mientras cargamos las entradas

---
+++ src/modules/daybook/components/IsLoadingComponent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="row justify-content-md-center">
+        <div class="col-3 alert-info text-center mt-5">
+            Espere por favor...
+            <h3 class="mt-2">
+                <i class="fa fa-spin fa-sync"></i>
+            </h3>
+        </div>
+    </div>
+</template>
--- src/modules/daybook/layouts/DaybookLayout.vue
+++ src/modules/daybook/layouts/DaybookLayout.vue
@@ -1,21 +1,32 @@
 <template>
     <NavbarComponent/>
-    <div class="d-flex">
-        <div class="col-4">
-            <EntryList/>
+    <template v-if="isLoading">
+        <IsLoadingComponent />
+    </template>
+    <template v-else>
+        <div class="d-flex">
+            <div class="col-4">
+                <EntryList/>
+            </div>
+            <div class="col">
+                <router-view></router-view>
+            </div>
         </div>
-        <div class="col">
-            <router-view></router-view>
-        </div>
-    </div>
+    </template>
 </template>
 <script>
     import {defineAsyncComponent} from 'vue'
-    import {mapActions} from 'vuex'
+    import {mapActions, mapState} from 'vuex'
     export default {
     components:{
         NavbarComponent: defineAsyncComponent(()=> import('../components/NavbarComponent.vue')),
-        EntryList: defineAsyncComponent(()=> import('../components/EntryListComponent.vue'))             
+        EntryList: defineAsyncComponent(()=> import('../components/EntryListComponent.vue')),
+        IsLoadingComponent: defineAsyncComponent(()=>import('../components/IsLoadingComponent.vue'))             
+    },
+
+    computed:{
+        //Getting the state of vuex using mapState
+        ...mapState('journal',['isLoading'])
     },
     methods: {
         // Fetching the actions of jounarl module
@@ -26,3 +37,9 @@
     },
 }
 </script>
+
+<style scoped>
+    .component-animation {
+        transition: all 3s ease-out;
+    }
+</style>
